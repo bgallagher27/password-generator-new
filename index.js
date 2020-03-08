502b013efe87fa35c5bf908d2a03db0553f58a75
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-08 16:16:09 -0400

    initial class setup

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const inquirer = require("inquirer");
 
+// class to concatenate the character responses
+class PasswordCharacters {
+    constructor(characters) {
+        this.characters = characters;
+
+    console.log(JSON.stringify(...characters));
+    };
+
+};
+
+class Password { 
+    constructor(length, inputs) {
+        this.length = length;
+        this.inputs = inputs;
+    };
+};
+
 // Initializing the prompt questions
 const questions = [
     {
@@ -11,11 +28,20 @@ const questions = [
         type: "checkbox",
         message: "Please choose the types of characters in your password:",
         choices: [
-            ("Uppercase Letters", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
-            ("Lowercase Letters", "abcdefghijklmnopqrstuvwxyz"),
-            ("Special Characters", "!@$%&*"),
-            ("Numbers", "0123456789"),
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "abcdefghijklmnopqrstuvwxyz",
+            "!@$%&*",
+            "0123456789",
         ],
         name: "parameters"
     }
 ]
+
+// Initializing the inquirer prompt
+inquirer.prompt(questions)
+
+//Formatting the inquirer responses
+.then(function(answers) {
+    let generatedInputs = new PasswordCharacters(answers.parameters);
+    //console.log(generatedPassword);
+});
