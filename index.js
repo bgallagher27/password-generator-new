6b9d26503012c60bfbdabe750714ec63fa77e999
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-08 17:12:22 -0400

    working functionality

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
 
+for(let i = 0; i <=this.length -1; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+}
+
 // class to concatenate the character responses
 class PasswordCharacters {
     constructor(characters) {
@@ -15,6 +19,18 @@ class Password {
     constructor(length, inputs) {
         this.length = length;
         this.inputs = inputs;
+    };
+
+    get show() {
+        return this.createPassword()
+    };
+
+    createPassword() {
+        let password = "";
+        for(let i = 0; i <=this.length -1; i++){
+            password = password + this.inputs.charAt(Math.floor(Math.random() * Math.floor(this.inputs.length - 1)));
+        }
+        return password;
     };
 };
 
@@ -44,7 +60,7 @@ inquirer.prompt(questions)
 //Formatting the inquirer responses
 .then(function(answers) {
     let generatedInputs = new PasswordCharacters(answers.parameters);
-    console.log(generatedInputs.chars);
-    //let generatedPassword = new Password(answers.characters, generatedInputs.chars);
-    //console.log(generatedPassword);
+    //console.log(generatedInputs.chars);
+    let generatedPassword = new Password(answers.characters, generatedInputs.chars);
+    console.log(generatedPassword.show);
 });
